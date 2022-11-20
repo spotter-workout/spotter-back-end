61328271b4c79c1c014516b3e06d47d78d7b0bc5
Pallav Marwaha <pallav.marwah@gmail.com>
2022-11-20 21:47:30 +0530

    added BaseUserManager to for custom User model

--- users/models.py
+++ users/models.py
@@ -1,8 +1,35 @@
 from django.db import models
 import uuid
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+class UserManager(BaseUserManager):  # type: ignore
+    """UserManager class."""
+
+    # type: ignore
+    def create_user(self, username: str, password: str = None) -> "User":
+        """Create and return a `User` with an email, username and password."""
+        if username is None:
+            raise TypeError("Users must have a username.")
+
+        user = self.model(username=username)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username: str, password: str) -> "User":  # type: ignore
+        """Create and return a `User` with superuser (admin) permissions."""
+        if password is None:
+            raise TypeError("Superusers must have a password.")
+
+        user = self.create_user(username, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_active = True
+        user.save()
+
+        return user
 
 
 class User(AbstractBaseUser):
@@ -16,3 +43,5 @@ class User(AbstractBaseUser):
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
